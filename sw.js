2b98f6de46edbb46e55524a30abbbd78e9c50971
Rory Madden <rory.madden@sociallymotivated.com>
2018-01-29 00:31:28 +0000

    JUst remove service worker caches for now

--- sw.js
+++ sw.js
@@ -6,33 +6,34 @@ const CACHE_NAME = "uxdx-2018-cache-v1";
 
 console.log("installing service worker");
 
-const filesToCache = [
+const urlsToCache = [
+  'style.css'
 
 ]
 
-self.addEventListener('install', function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', function (event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function (cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function (response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       return cache.match(event.request).then(function (response) {
+//         return response || fetch(event.request).then(function (response) {
+//           cache.put(event.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
