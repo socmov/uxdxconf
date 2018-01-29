2300f198ce77ba228e1ea6e7c1a0036c7a1dce0c
Rory Madden <rory.madden@sociallymotivated.com>
2018-01-29 00:20:48 +0000

    One more sw update - no caching

--- sw.js
+++ sw.js
@@ -7,9 +7,7 @@ const CACHE_NAME = "uxdx-2018-cache-v1";
 console.log("installing service worker");
 
 const filesToCache = [
-  "/",
-  "css/main.css",
-  "/index.html"
+
 ]
 
 self.addEventListener('install', function (event) {
