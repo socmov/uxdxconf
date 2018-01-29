5ce428d4fdb9e17893b09ea35b8588323b70b04e
Rory Madden <rory.madden@sociallymotivated.com>
2018-01-29 00:14:19 +0000

    Update SW to be static

--- sw.js
+++ sw.js
@@ -1,22 +1,9 @@
-//sw.js
----
-layout: null
----
-
-const staticCacheName = "uxdx-2018";
+const staticCacheName = "uxdx-2018-cache-v1";
 
 console.log("installing service worker");
 
 const filesToCache = [
   "/",
-  {% for page in site._includes %}
-    '{{ page.url }}',
-  {% endfor %}
-  {% for post in site.posts %}
-    '{{ post.url }}',
-  {% endfor %}
-
-  // can be automated rather than manual entries
   "css/main.css",
   "/index.html"
 ]
